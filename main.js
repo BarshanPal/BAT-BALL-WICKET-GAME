526399060789bfd8fd3f1a9edebea4461932ef17
BARSHAN PAL <barshanpal37@gmail.com>
2025-02-26 15:46:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,39 +8,48 @@ document.querySelector('#may').innerText = 'Choose one button to start the game'
 document.getElementById('ballButton').onclick = function() {
     if (randomColor === 'ball') {
         document.querySelector('#may').innerText = 'THE GAME IS DRAW';
+        alert("YOU CHOOSE BALL BUT COMPUTER CHOOSE BALL")
     } else if (randomColor === 'wicket') {
         document.querySelector('#may').innerText = 'YOU WON THE GAME';
+        alert("YOU CHOOSE BALL BUT COMPUTER CHOOSE WICKET")
     } else {
         document.querySelector('#may').innerText = 'YOU LOSE THE GAME';
+        alert("YOU CHOOSE BALL BUT COMPUTER CHOOSE BAT")
     }
     setTimeout(function() {
-        location.reload();  }, 100); 
+        location.reload();  }, 1000); 
 };
 
 document.getElementById('batButton').onclick = function() {
     if (randomColor === 'ball') {
         document.querySelector('#may').innerText = 'YOU WON THE GAME';
+        alert("YOU CHOOSE BAT BUT COMPUTER CHOOSE BALL")
     } else if (randomColor === 'wicket') {
         document.querySelector('#may').innerText = 'YOU LOSE THE GAME';
+        alert("YOU CHOOSE BAT BUT COMPUTER CHOOSE WICKET")
     } else {
         document.querySelector('#may').innerText = 'THE GAME IS DRAW';
+        alert("YOU CHOOSE BAT BUT COMPUTER CHOOSE BAT")
     }
     setTimeout(function() {
         location.reload(); 
-    }, 100); 
+    }, 1000); 
 };
 
 document.getElementById('wicketButton').onclick = function() {
     if (randomColor === 'ball') {
         document.querySelector('#may').innerText = 'YOU LOSE THE GAME';
+        alert("YOU CHOOSE WICKET BUT COMPUTER CHOOSE BALL")
     } else if (randomColor === 'wicket') {
         document.querySelector('#may').innerText = 'THE GAME IS DRAW';
+        alert("YOU CHOOSE WICKET BUT COMPUTER CHOOSE WICKET")
     } else {
         document.querySelector('#may').innerText = 'YOU WON THE GAME';
+        alert("YOU CHOOSE WICKET BUT COMPUTER CHOOSE BAT")
     }
     setTimeout(function() {
         location.reload(); 
-    }, 2000); 
+    }, 1000); 
 };
 
 
